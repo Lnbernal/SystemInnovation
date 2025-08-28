ced105f5ddcb7d41789a860144d4bcf8944a7fa4
nicolas bernal <luis.nicolas.bernal@gmail.com>
2025-08-27 23:14:48 -0500

    Se suben los archivos basicos al main

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    name = "Flask"
+    return render_template('index.html')
+
+@app.route('/index')
+def index():
+    return render_template('index2.html')
+
+
+if __name__ == "_main_":
+    app.run(debug=True)
