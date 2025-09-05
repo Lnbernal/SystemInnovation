8af718dc1053fb14e64a9fb856a65e6e20170bf9
Lnbernal <luis.nicolas.bernal@gmail.com>
2025-09-05 09:49:27 -0500

    Se verifica que todo este funcionando correctamente

--- LinealRegression.py
+++ LinealRegression.py
@@ -2,17 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 data = {
-    "Study Hours": [10, 15, 12, 8, 14, 5, 16, 7, 11, 13, 9, 4, 18, 3, 17, 6, 14, 2, 20, 1],
-    "Final Grade": [3.8, 4.2, 3.6, 3, 4.5, 2.5, 4.8, 2.8, 3.7, 4, 3.2, 2.2, 5, 1.8, 4.9, 2.7, 4.4, 1.5, 5, 1]
+    "Training Hours": [5, 8, 6, 4, 9, 3, 10, 7, 6, 8, 5, 2, 11, 1, 12, 3, 9, 2, 13, 1],
+    "Balanced Diet": [7, 8, 6, 5, 9, 4, 9, 7, 6, 8, 5, 3, 10, 2, 10, 4, 9, 2, 10, 1],
+    "Performance":   [7.5, 8.2, 7.0, 6.0, 9.0, 5.0, 9.5, 7.8, 7.2, 8.5, 6.8, 4.0, 9.8, 3.0, 10, 5.2, 9.1, 3.5, 10, 2.5]
 }
 
 df = pd.DataFrame(data)
 
-x = df[["Study Hours"]]
-y = df[["Final Grade"]]
+x = df[["Training Hours", "Balanced Diet"]]
+y = df[["Performance"]]
+
+
 model = LinearRegression()
 model.fit(x,y)
 
-def CalculateGrade(hours):
-    result =  model.predict([[hours]])[0]
+def Rendimiento(hours, diet):
+    result = model.predict([[hours, diet]])[0][0]
     return result
