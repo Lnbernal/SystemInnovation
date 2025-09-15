d4235353b6440f7e74b91b38038d31f23c0c68ea
JuanPinzon21 <juanchopinzon12c@gmail.com>
2025-09-14 19:41:56 -0500

    Recibo los cambios de mis compañeros y empiezo mi trabajo

--- RegresionLogistica.py
+++ RegresionLogistica.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('Agg') 
 import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
